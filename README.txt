b48a82f66ac09bac9a7b62b50bbd01998176cd59
Sandro Lucifora <sandro@kybernetik.com.de>
2023-11-15 17:13:40 +0100

    WPCasa Advanced Search 1.0.0

---
+++ README.txt
@@ -0,0 +1,56 @@
+=== WPCasa Advanced Search ===
+Contributors: wpsight, joehana
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7EVKNVA7QFXZ8
+Tags: real estate, realestate, search, advanced search, house, listing, listings, property, properties, wpcasa
+Requires at least: 4.0
+Tested up to: 5.8
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Display an expandable area with advanced options in WPCasa property search form.
+
+== Description ==
+
+By activating this add-on plugin an advanced search options will be added to the default WPCasa property search form. These options are placed in an expandable area that can be opened or closed by the click on the advanced button that is now displayed in the form.
+
+> Please notice that this plugin is an add-on for [WPCasa](https://wordpress.org/plugins/wpcasa/) and will NOT work without the core plugin.
+
+WPCasa is a WordPress solution that provides an intuitive way to manage property listings and create first-class real estate websites.
+
+* Website: [wpcasa.com](https://wpcasa.com)
+* Demo: [demo.wpcasa.com](http://demo.wpcasa.com)
+* Documentation: [docs.wpcasa.com](http://docs.wpsight.com)
+
+== Installation ==
+
+= Automatic Installation =
+
+Automatic installation is the easiest way to install WPCasa Advanced Search. Log into your WordPress admin and go to _WP-Admin > Plugins > Add New_.
+
+Then type "WPCasa Advanced Search" in the search field and click _Install Now_ once you've found the plugin.
+
+= Manual Installation =
+
+If you prefer to install the plugin manually, you need to download it to your local computer and upload the unzipped plugin folder to the `/wp-content/plugins/` directory of your WordPress installation. Then activate the plugin on _WP-Admin > Plugins_.
+
+== Frequently Asked Questions ==
+
+= Will this plugin work without WPCasa? =
+
+No, this is an add-on plugin for the WPCasa real estate framework and will not work without the core plugin.
+
+== Screenshots ==
+
+1. Advanced property search form (closed)
+2. Advanced property search form (open)
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Initial release
---
+++ wpcasa-advanced-search.php
@@ -0,0 +1,221 @@
+<?php
+/*
+Plugin Name: WPCasa Advanced Search
+Plugin URI: https://wpcasa.com/downloads/wpcasa-advanced-search
+Description: Display an expandable area with advanced options in WPCasa property search form.
+Version: 1.0.0
+Author: WPSight
+Author URI: http://wpsight.com
+Requires at least: 4.0
+Tested up to: 4.6
+Text Domain: wpcasa-advanced-search
+Domain Path: /languages
+
+	Copyright: 2015 Simon Rimkus
+	License: GNU General Public License v2.0 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+/**
+ *	WPSight_Advanced_Search class
+ */
+class WPSight_Advanced_Search {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+
+		// Define constants
+		
+		if ( ! defined( 'WPSIGHT_NAME' ) )
+			define( 'WPSIGHT_NAME', 'WPCasa' );
+		
+		if ( ! defined( 'WPSIGHT_DOMAIN' ) )
+			define( 'WPSIGHT_DOMAIN', 'wpcasa' );
+
+		define( 'WPSIGHT_ADVANCED_SEARCH_NAME', 'WPCasa Advanced Search' );
+		define( 'WPSIGHT_ADVANCED_SEARCH_DOMAIN', 'wpcasa-advanced-search' );
+		define( 'WPSIGHT_ADVANCED_SEARCH_VERSION', '1.0.0' );
+		define( 'WPSIGHT_ADVANCED_SEARCH_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+		define( 'WPSIGHT_ADVANCED_SEARCH_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
+		
+		// Cookie constants
+		
+		define( 'WPSIGHT_COOKIE_SEARCH_ADVANCED', WPSIGHT_DOMAIN . '_advanced_search' );
+
+		// Actions
+		
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'frontend_scripts' ) );
+		
+		// Filters
+		
+		add_filter( 'wpsight_get_search_fields', array( $this, 'get_advanced_search_fields' ) );
+
+	}
+
+	/**
+	 *	init()
+	 *
+	 *	Initialize the plugin when WPCasa is loaded.
+	 *
+	 *  @param	object	$wpsight
+	 *	@uses	do_action_ref_array()
+	 *  @return object	$wpsight->advanced_search
+	 *
+	 *	@since 1.0.0
+	 */
+	public static function init( $wpsight ) {
+		
+		if ( ! isset( $wpsight->advanced_search ) )
+			$wpsight->advanced_search = new self();
+
+		do_action_ref_array( 'wpsight_init_advanced_search', array( &$wpsight ) );
+
+		return $wpsight->advanced_search;
+	}
+
+	/**
+	 *	load_plugin_textdomain()
+	 *	
+	 *	Set up localization for this plugin
+	 *	loading the text domain.
+	 *	
+	 *	@uses	load_plugin_textdomain()
+	 *	@uses	plugin_basename()
+	 *
+	 *	@since 1.0.0
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( 'wpcasa-advanced-search', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 *	frontend_scripts()
+	 *	
+	 *	Register and enqueue scripts and css.
+	 *	
+	 *	@uses	wp_enqueue_style()
+	 *	@uses	wp_localize_script()
+	 *
+	 *	@since 1.0.0
+	 */
+	public function frontend_scripts() {
+		
+		// Script debugging?
+		$suffix = SCRIPT_DEBUG ? '' : '.min';
+
+		wp_enqueue_script( 'wpsight-listings-search-advanced', WPSIGHT_ADVANCED_SEARCH_PLUGIN_URL . '/assets/js/listings-search-advanced' . $suffix . '.js', array( 'jquery' ), WPSIGHT_ADVANCED_SEARCH_VERSION, true );
+		
+		// Localize scripts
+	
+		$data = array(
+			'cookie_path'				=> COOKIEPATH,
+			'cookie_search_advanced'	=> WPSIGHT_COOKIE_SEARCH_ADVANCED
+		);
+		
+		wp_localize_script( 'wpsight-listings-search-advanced', 'wpsight_localize', $data );
+
+	}
+
+	/**
+	 *	advanced_search_fields()
+	 *	
+	 *	Register and enqueue scripts and css.
+	 *	
+	 *	@uses	wpsight_sort_array_by_priority()
+	 *
+	 *	@since 1.0.0
+	 */
+	public function get_advanced_search_fields( $fields_default ) {
+		
+		// Set advanced form fields
+		
+		$fields_advanced = array(
+
+			'min' => array(
+				'label' 		=> __( 'Price (min)', 'wpcasa-advanced-search' ),
+				'key'			=> '_price',
+				'type' 			=> 'text',
+				'data_compare' 	=> '>=',
+				'data_type' 	=> 'numeric',
+				'advanced'		=> true,
+				'class'			=> 'width-1-4',
+		    	'priority'		=> 90
+			),
+
+			'max' => array(
+				'label' 		=> __( 'Price (max)', 'wpcasa-advanced-search' ),
+				'key'			=> '_price',
+				'type' 			=> 'text',
+				'data_compare' 	=> '<=',
+				'data_type' 	=> 'numeric',
+				'advanced'		=> true,
+				'class'			=> 'width-1-4',
+		    	'priority'		=> 100
+			),
+
+			'orderby' => array(
+				'label'			=> __( 'Order by', 'wpcasa-advanced-search' ),
+				'type' 			=> 'select',
+				'data' 			=> array(
+					'date'  => __( 'Date', 'wpcasa-advanced-search' ),
+					'price' => __( 'Price', 'wpcasa-advanced-search' ),
+					'title'	=> __( 'Title', 'wpcasa-advanced-search' )
+				),
+				'default'		=> 'date',
+				'advanced'		=> true,
+				'class'			=> 'width-1-4',
+		    	'priority'		=> 110
+			),
+
+			'order' => array(
+				'label'			=> __( 'Order', 'wpcasa-advanced-search' ),
+				'type' 			=> 'select',
+				'data' 			=> array(
+					'asc'  => __( 'asc', 'wpcasa-advanced-search' ),
+					'desc' => __( 'desc', 'wpcasa-advanced-search' )
+				),
+				'default'		=> 'desc',
+				'advanced'		=> true,
+				'class'			=> 'width-1-4',
+		    	'priority'		=> 120
+			),
+			
+			'feature' => array(
+				'label'			=> '',
+				'data' 			=> array(
+					// get_terms() options
+					'taxonomy'	=> 'feature',
+			    	'orderby'	=> 'count', 
+					'order'		=> 'DESC',
+					'operator'	=> 'AND', // can be OR
+					'number'	=> 8
+				),
+				'type' 			=> 'taxonomy_checkbox',
+				'advanced'		=> true,
+				'class'			=> 'width-auto',
+		    	'priority'		=> 130
+			)
+			
+		);
+		
+		$fields_advanced = apply_filters( 'wpsight_get_advanced_search_fields', $fields_advanced, $fields_default );
+		
+		// Merge default and advanced search
+		$fields = array_merge( $fields_default, $fields_advanced );
+		
+		// Apply filter and sort array by priority  
+		return wpsight_sort_array_by_priority( $fields );
+
+	}
+	
+}
+
+// Initialize plugin on wpsight_init
+add_action( 'wpsight_init', array( 'WPSight_Advanced_Search', 'init' ) );
